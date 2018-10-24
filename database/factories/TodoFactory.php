e1446e22d40515c170cedaa80ab1b29bfea050a8
vvmk <vincentmasiello@gmail.com>
2018-10-24 19:46:47 -0400

    Set up Board, Todo factories

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -4,6 +4,13 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Todo::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+        'board_id' => function () {
+            return factory('App\Board')->create()->id;
+        },
+        'description' => $faker->sentence,
+        'completed' => false,
     ];
 });
