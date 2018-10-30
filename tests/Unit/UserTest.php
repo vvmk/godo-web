4cabfe7461d80089bf57e7bf8cdb9b95a4eecd40
vvmk <vincentmasiello@gmail.com>
2018-10-29 21:05:23 -0400

    Write a failing test to remind me where I stopped

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -10,9 +10,19 @@ class UserTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_user_path() {
-        $user = factory('App\User')->create();
+    public function setUp() {
+        parent::setUp();
 
-        $this->assertEquals('/users/' . $user->name, $user->path());
+        $this->user = factory('App\User')->create();
+    }
+
+    public function test_user_path() {
+        $this->assertEquals('/users/' . $this->user->name, $this->user->path());
+    }
+
+    public function test_user_has_a_profile_page() {
+        $response = $this->get('/users/' . $this->user->id);
+
+       $response->assertSee($this->user->name); 
     }
 }
