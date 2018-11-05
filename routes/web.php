534766ef1d2cc73d90ad804945ca7d01738dee9e
vvmk <vincentmasiello@gmail.com>
2018-11-04 21:06:51 -0500

    Name a route and add root route

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,15 @@
 
 Auth::routes();
 
+Route::get('/', 'HomeController@welcome');
+
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/boards', 'BoardController@index');
 Route::get('/boards/{name}', 'BoardController@show');
 Route::post('/boards/{name}/todos', 'TodoController@create');
 
-Route::get('/users/{id}', 'HomeController@show');
+Route::get('/users/{id}', 'HomeController@show')->name('profile');
     
-Route::put('/todos/{id}/complete', 'TodoController@complete');
+Route::put('/todos/{todo}/complete', 'TodoController@complete');
 
