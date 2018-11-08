d07b6a9f6dd1a3df3d373021f9b01db264c5ea27
vvmk <vincentmasiello@gmail.com>
2018-11-07 22:48:37 -0500

    Fix boards@index and style it a little

--- resources/views/boards/show.blade.php
+++ resources/views/boards/show.blade.php
@@ -1,24 +1,41 @@
 @extends('layouts.master')
 @section('content')
 
-<div class="content">
-    <div class="panel">
-        <div class="panel-heading">
-            {{ $board->name }} by <a href="{{ $board->creator->path() }}">{{ $board->creator->name }}</a>
-        </div>
-        @foreach($todos as $todo)
-            <a class="panel-block">
-                <form method="POST" action="/todos/{{ $todo->id }}">
-                    @method('PATCH')
-                    @csrf
-                    <label class="checkbox" for="completed">
-                        <input type="checkbox" name="completed">
-                        {{ $todo->description }}
-                    </label>
-                </form>
+<h3 class="subtitle is-3">
+    {{ $board->name }} by <a href="{{ $board->creator->path() }}">{{ $board->creator->name }}</a>
+</h3>
 
-                <a class="" href="{{ $todo->creator->path() }}">{{ $todo->creator->name }}</a>added {{ $todo->created_at->diffForHumans() }}
-            </a>
+<div class="columns">
+
+    <div class="column is-2 has-background-light">
+    </div>
+
+    <div class="column is-8">
+        @foreach($board->todos as $todo)
+
+        <div class="level">
+            <div class="level-left">
+
+                <div class="level-item">
+                    <form method="POST" action="/todos/{{ $todo->id }}">
+                        @method('PATCH')
+                        @csrf
+                        <label class="checkbox" for="completed">
+                            <input type="checkbox" name="completed">
+                            {{ $todo->description }}
+                        </label>
+                    </form>
+
+                </div>
+            </div>
+
+            <div class="level-right">
+                <div class="level-item">
+                    <a class="level-item" href="{{ $todo->creator->path() }}">{{ $todo->creator->name }}</a>&nbsp;added {{ $todo->created_at->diffForHumans() }}
+                </div>
+            </div>
+        </div>
+
         @endforeach
     </div>
 </div>
