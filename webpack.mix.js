962230f575e06b76a167c4b6b27d5b06d109dcd5
vvmk <vincentmasiello@gmail.com>
2018-11-07 21:36:09 -0500

    Remove browser-sync
    I'm investigating why it was crashing my browser window anytime there
    were build errors. Likely a config issue, since I grabbed it from
    someone else's config and shoe-horned it in.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,10 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-   .extract([
-      'vue',
-      'axios',
-      'bulma',
-   ])
-   .sass('resources/sass/app.scss', 'public/css')
-   .browserSync({
-      proxy: 'godo-web.test'
-   })
-   .version();
+    .extract([
+        'vue',
+        'axios',
+        'bulma',
+    ])
+    .sass('resources/sass/app.scss', 'public/css')
+    .version();
