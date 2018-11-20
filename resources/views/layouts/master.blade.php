4efc9dc182f9b25739a3ff5d8fa6f257b1580faa
vvmk <vincentmasiello@gmail.com>
2018-11-19 23:59:23 -0500

    Remove the hero thing

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,17 +19,6 @@
                 @include('layouts.nav')
             </hero-nav>
 
-            <section class="hero is-dark">
-                <div class="hero-body">
-                    <div class="container has-text-left">
-                        <h1 class="title is-3">
-                            //GODO:
-                        </h1>
-                        <h3 class="subtitle is-5">CLI managed todo lists with a singe point of truth.</h3>
-                    </div>
-                </div>
-            </section>
-
             <section class="section">
                 <main class="container">
                     @yield('content')
