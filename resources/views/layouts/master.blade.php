89122ec883e3d34e3eb4974c09a4cdb73f18cf19
vvmk <vincentmasiello@gmail.com>
2018-12-01 12:05:59 -0500

    Extract hero to a partial

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,16 +19,7 @@
                 @include('layouts.nav')
             </hero-nav>
 
-            <section class="hero is-dark">
-                <div class="hero-body">
-                    <div class="container has-text-left">
-                        <h1 class="title is-3">
-                            //GODO:
-                        </h1>
-                        <h3 class="subtitle is-5">CLI managed todo lists with a singe point of truth.</h3>
-                    </div>
-                </div>
-            </section>
+            @include('layouts.hero')
 
             <section class="section">
                 <main class="container">
