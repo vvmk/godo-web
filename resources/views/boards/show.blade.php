304d217399d0b992a2d39f9bd5d6fa35ea5cd1ca
vvmk <vincentmasiello@gmail.com>
2018-12-05 18:39:42 -0500

    Fix checkbox elements being re-used on cleanup

--- resources/views/boards/show.blade.php
+++ resources/views/boards/show.blade.php
@@ -71,7 +71,7 @@
                             </div>
                         </div>
 
-                        <div v-for="todo in todos" class="level">
+                        <div v-for="todo in todos" :key="todo.id" class="level">
 
                             <div class="level-left">
                                 <div class="level-item">
