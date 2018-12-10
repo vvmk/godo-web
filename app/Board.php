0a18517f9a35eb88dd84c83d36207f7d4c3eedbd
vvmk <vincentmasiello@gmail.com>
2018-12-09 23:58:01 -0500

    Order todos by decending created_at

--- app/Board.php
+++ app/Board.php
@@ -23,7 +23,7 @@ class Board extends Model
     }
 
     public function todos() {
-        return $this->hasMany(Todo::class);
+        return $this->hasMany(Todo::class)->sortByDesc('created_at');
     }
 
     public function addTodo($attributes) {
